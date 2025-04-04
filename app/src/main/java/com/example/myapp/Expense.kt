b3907e0b0d5958e7df5be75482c3f380ed3c23d8
VarinderPalSingh <w0834091@myscc.ca>
2025-04-04 00:51:41 -0400

    created branch assignment 10 and add the dependencies and added the new fields in expense data.

--- app/src/main/java/com/example/myapp/Expense.kt
+++ app/src/main/java/com/example/myapp/Expense.kt
@@ -1,7 +1,12 @@
 package com.example.myapp
 
+import android.icu.util.Currency
+
 data class Expense(
     val name: String,
     val amount: String,
-    val date: String
+    val date: String,
+    val currency: Currency,
+    val convertedCost: Double
+
 )
